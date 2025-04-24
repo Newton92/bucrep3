266870db5454c69cca236a874df756d01cfacff9
Yannick ABOH <yannickabohthierry@gmail.com>
2025-04-24 18:13:56 +0100

    Xlast update

---
+++ entrypoint.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# python manage.py collectstatic --noinput
+python manage.py makemigrations
+python manage.py migrate
+# python manage.py runserver 0.0.0.0:8005
+gunicorn bucrep.wsgi:application --bind 0.0.0.0:8000
