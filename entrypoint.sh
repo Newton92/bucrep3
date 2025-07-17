821dd605a0a444c22d8bce4f80fc0a124fa4bb48
Yannick ABOH <yannickabohthierry@gmail.com>
2025-07-17 22:57:03 +0100

    Mise a jour du fichier entry.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,5 @@
 python manage.py makemigrations
 python manage.py migrate
 # python manage.py runserver 0.0.0.0:8005
+python manage.py collectstatic --noinput
 gunicorn bucrep.wsgi:application --bind 0.0.0.0:8000
